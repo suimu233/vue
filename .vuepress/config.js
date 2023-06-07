7e5f92cea05c6640c56fae4eadc0d3c4b67bf57f
suimu233 <2117902243@qq.com>
2023-06-07 19:08:18 +0800

    去除评论功能

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -71,13 +71,13 @@ module.exports = {
         "link": "https://smallleaf233.github.io"
       }
     ],
-    "valineConfig": {
-      "appId": "xxx",// your appId
-      "appKey": "xxx", // your appKey
-      "avatar": "", //
-      "enableQQ": true, //启用昵称框自动获取QQ昵称和QQ头像
-      "requiredFields": ['nick', 'mail'], //设置必填项
-    },
+    // "valineConfig": {
+    //   "appId": "xxx",// your appId
+    //   "appKey": "xxx", // your appKey
+    //   "avatar": "", //
+    //   "enableQQ": true, //启用昵称框自动获取QQ昵称和QQ头像
+    //   "requiredFields": ['nick', 'mail'], //设置必填项
+    // },
     "logo": "/logo.png",
     // "huawei": true, //首页出现华为文案
     "search": true,
